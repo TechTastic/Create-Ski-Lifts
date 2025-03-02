1e790576ab75f9bd3fe6c41fc22135a22b5c34d7
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-03-02 09:49:20 -0600

    Initial Stuff

---
+++ src/main/java/io/github/techtastic/createskilifts/CreateSkiLifts.java
@@ -0,0 +1,38 @@
+package io.github.techtastic.createskilifts;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(CreateSkiLifts.MOD_ID)
+public class CreateSkiLifts {
+    public static final String MOD_ID = "createskilifts";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CreateSkiLifts(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
